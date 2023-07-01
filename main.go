c919038c007d84d6756bde124fa30222862428b7
Geovanny de Avelar Carneiro <geovanny.carneiro@iacit.com.br>
2023-07-01 11:41:00 -0300

    Env methods

--- main.go
+++ main.go
@@ -5,11 +5,14 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"os"
 	"os/exec"
+	"strconv"
 	"strings"
 
 	"github.com/go-chi/chi"
 	"github.com/gorilla/handlers"
+	"github.com/joho/godotenv"
 	"golang.org/x/net/html"
 
 	log "github.com/sirupsen/logrus"
@@ -18,6 +21,10 @@ import (
 var MAPSERVER_EXEC = ""
 
 func main() {
+	log.SetFormatter(&log.TextFormatter{
+		FullTimestamp: true,
+	})
+
 	LoadDotEnvFile(".env")
 
 	MAPSERVER_EXEC = GetMapServerPath()
@@ -34,9 +41,10 @@ func main() {
 
 	port := GetApiPort()
 	host := fmt.Sprintf(":%d", port)
-	log.Info("Listening at " + host + GetLocalHost())
 
 	handler := handlers.CORS(originsOk, headersOk, methodsOk)(router)
+
+	log.Info("Listening at " + host)
 
 	http.ListenAndServe(host, handler)
 }
@@ -106,4 +114,88 @@ func parseMapServerErrorMessage(htmlResponse string) (string, error) {
 	}
 
 	return "", errors.New("cannot parse MapServer error message")
+}
+
+func LoadDotEnvFile(path string) {
+	if err := godotenv.Load(path); err != nil {
+		log.Println("Error loading env file")
+	}
+}
+
+func GetMapServerPath() string {
+	envVar := os.Getenv("MAPSERVER_WRAPPER_MAP_SERV_EXEC_PATH")
+
+	if envVar == "" {
+		log.Warn("MAPSERVER_WRAPPER_ALLOWED_ORIGINS environment variable is not defined. ")
+		panic("MAPSERVER_WRAPPER_MAP_SERV_EXEC_PATH is no defined")
+	}
+
+	return envVar
+}
+
+func GetAllowedOrigins() []string {
+	envVar := os.Getenv("MAPSERVER_WRAPPER_ALLOWED_ORIGINS")
+
+	if envVar != "" {
+		return strings.Split(envVar, ",")
+	}
+
+	log.Warn("MAPSERVER_WRAPPER_ALLOWED_ORIGINS environment variable is not defined. Accepting only local connections")
+
+	return []string{GetLocalHost()}
+}
+
+func GetApiPort() int {
+	envVar := os.Getenv("MAPSERVER_WRAPPER_PORT")
+
+	if envVar != "" {
+		port, err := strconv.Atoi(envVar)
+
+		if err == nil {
+			return port
+		}
+
+		log.Warn("Cannot parse MAPSERVER_WRAPPER environment variable. Port must be an integer.")
+	}
+
+	log.Warn("MAPSERVER_WRAPPER is not defined. Using default port 8000.")
+
+	return 8000
+}
+
+func GetLocalHost() string {
+	logLevel := log.GetLevel()
+
+	log.SetLevel(0)
+	port := GetApiPort()
+	log.SetLevel(logLevel)
+
+	return fmt.Sprintf("http://localhost:%d", port)
+}
+
+func GetRootPath() string {
+	root := os.Getenv("MAPSERVER_WRAPPER_BASE_PATH")
+
+	if root != "" && len(root) > 0 {
+		if root[0] == '/' {
+			return root
+		}
+	}
+
+	log.Warn("MAPSERVER_WRAPPER_BASE_PATH environment variable is not defined. Default is /")
+
+	return "/"
+}
+
+func GetCachePath() string {
+	path := os.Getenv("MAPSERVER_WRAPPER_CACHE_PATH")
+
+	if path != "" {
+		return path
+	}
+
+	log.Warn("MAPSERVER_WRAPPER_CACHE_PATH environment variable is not defined." +
+		"Cached tiles will be stored in ./cache folder")
+
+	return "cache"
 }
